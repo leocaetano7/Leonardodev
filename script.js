ce37f71c24c619d983f557bbfeea92e84a053e67
Leonardo Caetano <129470226+leocaetano7@users.noreply.github.com>
2023-04-28 02:04:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 
 function escrevendoLetra(){
     function ativaLetra(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-    }, 75 * i);
-})
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        })
+    }
 
-const titulo = document.querySelector('.digitando')
-ativaLetra(titulo);
-
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
 }
-}
 
+// chama a função escrevendoLetra() aqui
+escrevendoLetra();
 
-
-const ativaMenu = document.querySelector('.fa-bars')
-const navMenu = document.querySelector('header .nave')
+const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .nave');
 
 ativaMenu.addEventListener('click', ()=>{
-    ativaMenu.classList.toggle('fa-x')
-    navMenu.classList.toggle('open')
-})
-
+    ativaMenu.classList.toggle('fa-x');
+    navMenu.classList.toggle('open');
+});
